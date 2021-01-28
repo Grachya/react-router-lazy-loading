728bc4fb41b96a80fcf6e512857c9da56757a7fe
Грачья Дарбинян <gracadarbinan@MacBook-Pro-Graca.local>
2021-01-28 19:49:46 +0300

    react-router-lazy-loading

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import React, { Suspense, lazy } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+
+const Home = lazy(() => import('./routes/Home/Home'));
+const About = lazy(() => import('./routes/About/About'));
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="wrapper">
+        <Suspense fallback={<div>Загрузка...</div>}>
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route path="/about" component={About}/>
+          </Switch>
+        </Suspense>
+      </div>
+    </Router>
     </div>
   );
 }
